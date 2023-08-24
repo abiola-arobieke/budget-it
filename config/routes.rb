e2ed4d5faee06c9fdceab742a190210bc6ad6943
abiola-arobieke <abiola.arobieke@gmail.com>
2023-08-24 20:40:39 +0100

    Update routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,31 @@
 Rails.application.routes.draw do
+  # get 'expense_items/index'
+  # get 'expense_items/show'
+  # get 'expense_items/new'
+  # get 'expense_items_categories/index'
+  # get 'expense_items_categories/show'
+  # get 'expense_items_categories/new'
+  # get 'categories/index'
+  # get 'categories/show'
+  # get 'categories/new'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: "home#index"
+  # root to: "home#index"
+  # devise_scope :user do
+  #   authenticated :user do
+  #     root to: 'categories#index', as: :authenticated_root
+  #   end
+
+  #   unauthenticated do
+      root 'home#index'
+  #   end
+  # end
+
+  
+  resources :categories, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+    resources :expense_items, only: [:index, :show, :new, :create, :edit, :update, :destroy] 
+  end
 end
