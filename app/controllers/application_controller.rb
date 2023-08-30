11d7fc990f4d56327f35b726cec76b1e814a9aa7
abiola-arobieke <abiola.arobieke@gmail.com>
2023-08-30 16:10:51 +0100

    Update application_controller.rb file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,22 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  def after_sign_in_path_for(_resource)
+    categories_path
+  end
+
+  def after_sign_out_path_for(_resource)
+    root_path
+  end
+
+  def after_sign_up_path_for(_resource)
+    new_user_session_path # <- Path you want to redirect the user to.
+  end
+
+  def redirect_user
+    redirect_to '/404'
+  end
+
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
     devise_parameter_sanitizer.permit(:account_update) do |u|
