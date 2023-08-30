5e635ab5f5259b2c158a767c56a6d1b977cfa069
abiola-arobieke <abiola.arobieke@gmail.com>
2023-08-30 23:23:54 +0100

    Fix linters error

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,7 @@
 Rails.application.routes.draw do
   # get 'home/index'
-  # get 'expense_items/index'
-  # get 'expense_items/show'
-  # get 'expense_items/new'
-  # get 'expense_items_categories/index'
-  # get 'expense_items_categories/show'
-  # get 'expense_items_categories/new'
-  # get 'categories/index'
-  # get 'categories/show'
-  # get 'categories/new'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  # root "home#index"
   devise_scope :user do
     authenticated :user do
       root to: 'categories#index', as: :authenticated_root
@@ -24,9 +11,6 @@ Rails.application.routes.draw do
       root 'home#index'
     end
   end
-  #If no route matches
-  
-  # match '*path', via: :all, to: redirect('/404')
 
   resources :categories, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
     resources :expense_items, only: [:index, :show, :new, :create, :edit, :update, :destroy] 
